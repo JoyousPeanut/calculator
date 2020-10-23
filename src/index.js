a6a1210287746a918ca6a4e9818628383ba05acc
JoyousPeanut <71414370+JoyousPeanut@users.noreply.github.com>
2020-10-23 13:50:18 +1000

    Added Styles/Basic Button Presses

--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
-import "./styles.css";
+// Calculator Animations
+const calc = () => {
+  // Declarations
+  let calcNumbers = [];
+  const screen = document.getElementById("calcScreen");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  // Calculator operations
+  const calculator = {
+    divide: function (...numbers) {
+      return numbers / numbers;
+    },
+    subtract: function (args) {
+      let result = args.reduce((acc, curr) => {
+        return acc - curr;
+      });
+      return result;
+    },
+    multiply: function (args) {
+      // To Fix
+      let result = args.reduce((acc, curr) => {
+        return acc * curr;
+      });
+      return result;
+    },
+    addition: function (args) {
+      let result = args.reduce((acc, curr) => {
+        return acc + curr;
+      });
+      return result;
+    }
+  };
+
+  // Hover Highlight
+  let numbBtn = document.getElementsByClassName("numberBtn");
+  for (let i = 0; i < numbBtn.length; i++) {
+    numbBtn[i].addEventListener("mouseover", (event) => {
+      event.target.classList.toggle("btnHover");
+    }); // Button Press Animation
+    numbBtn[i].addEventListener("click", (event) => {
+      event.target.classList.toggle("shadow");
+      let pressedBtn = event.target;
+      console.log(pressedBtn.innerHTML);
+
+      calcNumbers.push(parseInt(pressedBtn.innerHTML, 10)); // Pushes pressed buttons to array
+      screen.innerHTML = calcNumbers.join("");
+      setTimeout(() => {
+        event.target.classList.toggle("shadow"); // Toggles Shadow on Mouse Hover
+      }, 300);
+    }); // Removes Hover Highlight
+    numbBtn[i].addEventListener("mouseout", (event) => {
+      event.target.classList.toggle("btnHover");
+    });
+  }
+};
+
+calc();
